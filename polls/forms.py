cb209a18eda6a9b1387f609cb8519902b54b2061
girishsontakke <girishsontakke7@gmail.com>
2020-10-13 16:50:59 +0530

    add placeholder in form field

--- polls/forms.py
+++ polls/forms.py
@@ -1,8 +1,10 @@
 from django import forms
+from django.forms import widgets
 from .models import Question, Choice
 
 
 class QuestionCreationForm(forms.Form):
-    question_text = forms.CharField(max_length=100)
+    question_text = forms.CharField(max_length=100, widget=widgets.TextInput(
+        attrs={'placeholder': 'Enter your question'}))
     choice1 = forms.CharField()
     choice2 = forms.CharField()
