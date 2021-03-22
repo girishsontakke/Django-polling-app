209252148de62e6e78340464459dd066edb38111
girishsontakke <girishsontakke7@gmail.com>
2021-03-22 11:20:45 +0530

    test and resolve recently published questions bug for future questions

--- polls/models.py
+++ polls/models.py
@@ -7,11 +7,12 @@ import datetime
 # Create your models here.
 
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=200, null=False)
     pub_date = models.DateTimeField(default=timezone.now)
 
     def was_published_recently(self):
-        return self.pub_date >= timezone.now()-datetime.timedelta(days=1)
+        now = timezone.now()
+        return now >= self.pub_date >= now-datetime.timedelta(days=1)
 
     def __str__(self):
         return self.question_text
--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+import datetime
 
 # Create your tests here.
+
+
+class PollTest(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        question_text = "Test Question"
+        question = Question(question_text=question_text, pub_date=timezone.now() +
+                            datetime.timedelta(days=30))
+        self.assertIs(question.was_published_recently(), False)
