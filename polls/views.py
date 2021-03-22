c4266d9a19aa593930c10be8c2f271f3946ad518
girishsontakke <girishsontakke7@gmail.com>
2021-03-22 11:21:32 +0530

    send alert massage on any actions

--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse
 from django.views import generic
-from django.contrib.auth.models import User
+from django.contrib import messages
 
 from .models import Question, Choice
 from .forms import QuestionCreationForm
@@ -32,9 +32,9 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except(KeyError, Choice.DoesNotExist):
+        messages.warning(request, "You haven't selected any choice")
         return render(request, 'polls/detail.html', {
             'question': question,
-            'error_message': "You didn't select a choice.",
         })
     else:
         selected_choice.votes = F('votes') + 1
