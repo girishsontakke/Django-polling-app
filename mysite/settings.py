ff55c870add55cd0e399f11d19b3537bd7d15af3
girishsontakke <girishsontakke7@gmail.com>
2021-03-22 11:19:17 +0530

    install crispy_forms

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "crispy_forms"
 ]
 
 MIDDLEWARE = [
@@ -107,3 +108,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
